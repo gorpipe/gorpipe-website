6db303f83ebb933e6b04d1c99e8043ac8792780a
Hlynur Tryggvason <hlynur@wuxinextcode.com>
2020-02-13 17:01:01 +0000

    Update content

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -11,6 +11,63 @@ const Footer = () => {
           <GORLogoOutlineText title="GOR" />
         </a>
       </Link>
+
+      <div>
+        Learn more
+        <ul>
+          <li>
+            <a>Documentation</a>
+          </li>
+          <li>
+            <a>Tutorials</a>
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        Legal
+        <ul>
+          <li>
+            <a>Terms</a>
+          </li>
+          <li>
+            <a>Privacy</a>
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        Support
+        <ul>
+          <li>
+            <a>GitHub</a>
+          </li>
+          <li>
+            <a>Release Notes</a>
+          </li>
+          <li>
+            <a>Issue Tracker</a>
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        Company
+        <ul>
+          <li>
+            <a>About</a>
+          </li>
+          <li>
+            <a>Blog</a>
+          </li>
+          <li>
+            <a>Contact</a>
+          </li>
+          <li>
+            <a>Community</a>
+          </li>
+        </ul>
+      </div>
     </footer>
   );
 };
