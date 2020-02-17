9c4e7a5b984533c54ffb3ca3af164f683fdcf4cb
Hlynur Tryggvason <hlynur@wuxinextcode.com>
2020-02-17 15:04:47 +0000

    Add social media sharing meta tags

---
+++ components/DocHead.js
@@ -0,0 +1,40 @@
+import Head from "next/head";
+
+const defaults = {
+  title: "GORpipe",
+  description: "Genomic Insights at Scale",
+  shareImg: "https://gorpipe.org/gor-pipe-share-img.png"
+};
+
+const DocHead = props => {
+  const options = { ...props, ...defaults };
+
+  return (
+    <Head>
+      <title>{options.title}</title>
+
+      <meta
+        key="description"
+        name="description"
+        content={options.description}
+      />
+
+      <meta key="og:title" property="og:title" content={options.title} />
+      <meta
+        key="og:description"
+        property="og:description"
+        content={options.description}
+      />
+      <meta key="og:image" property="og:image" content={options.shareImg} />
+
+      <meta name="twitter:title" content={options.title} />
+      <meta name="twitter:description" content={options.description} />
+      <meta name="twitter:image" content={options.shareImg} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@wuxinextcode" />
+      <meta name="twitter:creator" content="@wuxinextcode" />
+    </Head>
+  );
+};
+
+export default DocHead;
--- components/Layout.js
+++ components/Layout.js
@@ -1,20 +1,12 @@
-import Head from "next/head";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import DocHead from "./DocHead";
 
 import s from "./Layout.scss";
 
 const Layout = props => (
   <div className={s.Layout}>
-    <Head>
-      <title key="defaulttitle">GORpipe</title>
-
-      <meta
-        key="defaultdesc"
-        name="description"
-        content="Genomic Insights at Scale"
-      />
-    </Head>
+    <DocHead />
     <Header />
     <main className={s.Layout__main}>{props.children}</main>
     <Footer />
