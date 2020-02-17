e190be4d1b0b43331fe6ce29b5d4358ba904dde2
Hlynur Tryggvason <hlynur@wuxinextcode.com>
2020-02-17 14:14:03 +0000

    Add favicon and meta tags

--- components/Layout.js
+++ components/Layout.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 
@@ -5,6 +6,15 @@ import s from "./Layout.scss";
 
 const Layout = props => (
   <div className={s.Layout}>
+    <Head>
+      <title key="defaulttitle">GORpipe</title>
+
+      <meta
+        key="defaultdesc"
+        name="description"
+        content="Genomic Insights at Scale"
+      />
+    </Head>
     <Header />
     <main className={s.Layout__main}>{props.children}</main>
     <Footer />
